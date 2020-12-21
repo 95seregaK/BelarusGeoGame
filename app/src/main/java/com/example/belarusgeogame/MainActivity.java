a35fdda59eb7d854dd2ade3ee297fdec39560480
kazakovsr <protectedserega95@gmail.com>
2020-12-21 16:53:25 +0300

    My commitement

--- app/src/main/java/com/example/belarusgeogame/MainActivity.java
+++ app/src/main/java/com/example/belarusgeogame/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.belarusgeogame;
 
-import android.graphics.Path;
 import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,61 +11,94 @@ import com.cocoahero.android.geojson.Feature;
 import com.cocoahero.android.geojson.FeatureCollection;
 import com.cocoahero.android.geojson.GeoJSON;
 import com.cocoahero.android.geojson.GeoJSONObject;
-import com.cocoahero.android.geojson.Point;
+import com.cocoahero.android.geojson.MultiPolygon;
 import com.cocoahero.android.geojson.Polygon;
 import com.cocoahero.android.geojson.Position;
 import com.cocoahero.android.geojson.Ring;
 
+import org.json.JSONException;
+
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private MyView myView;
-    private int scale = 100;
+    private int scale = 2000;
+    private float la0 = (float) toRad(27);
+    private float fi0 = (float) toRad(48);
+
+    public static double toRad(double fi) {
+        return fi / 180 * Math.PI;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Path path = new Path();
         myView = findViewById(R.id.my_view);
-
+        List<Country> countries = new ArrayList<>();
         try {
-            InputStream stream = getResources().openRawResource(R.raw.europe);
-
+            InputStream stream = getResources().openRawResource(R.raw.world_l);
             GeoJSONObject geoJSON = GeoJSON.parse(stream);
-            String text = "";
-            Log.d("FileInputStream", "" + (geoJSON instanceof FeatureCollection));
-            FeatureCollection featureCollection = (FeatureCollection) (geoJSON);
-            List<Feature> features = featureCollection.getFeatures();
-            for (Feature feature : features) {
-                if (feature.getGeometry().getType() == "Point") {
-                    Point point = (Point) feature.getGeometry();
-                    point.getPosition().getLatitude();
-                    text += point.getPosition().getLatitude() + " " + point.getPosition().getLongitude() + " " + point.getPosition().getAltitude();
-                } else if (feature.getGeometry().getType() == "Polygon") {
-                    Polygon polygon = (Polygon) feature.getGeometry();
-                    List<Ring> rings = polygon.getRings();
-                    for (Ring ring : rings) {
-                        PointF point = project(ring.getPositions().get(0));
-                        path.moveTo(point.x, point.y);
-                        for (Position position : ring.getPositions()) {
-                            point = project(position);
-                            path.lineTo(point.x, point.y);
-                        }
-                    }
-                }
-            }
-            //textView.setText(text);
-            myView.addPath(path);
+            readCountries(geoJSON, countries);
+            //Log.d("FileInputStream", "" + (geoJSON instanceof FeatureCollection));
+            myView.addCountries(countries);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public PointF project(Position position) {
-        float x = (float) (scale * (position.getLongitude() - 23));
-        float y = (float) (1.5 * scale * (57 - position.getLatitude()));
-        return new PointF(x, y);
+        return project(toRad(position.getLongitude()), toRad(position.getLatitude()));
+    }
+
+    public PointF project(double la, double fi) {
+        double x = Math.sin((la - la0) * Math.sin(fi)) / Math.tan(fi);
+        double y = fi - fi0 + (1 - Math.cos((la - la0) * Math.sin(fi))) / Math.tan(fi);
+        return new PointF((float) x * scale, (float) (1 - y) * scale);
+    }
+
+    public void readPolygon(Polygon polygon, List<PointF[]> dest) {
+        List<Ring> rings = polygon.getRings();
+        for (Ring ring : rings) {
+            List<Position> positions = ring.getPositions();
+            int n = positions.size();
+            PointF[] points = new PointF[n];
+            for (int i = 0; i < positions.size(); i++) {
+                points[i] = project(positions.get(i));
+            }
+            dest.add(points);
+        }
+    }
+
+    public void readCountries(GeoJSONObject geoJSON, List<Country> countries) throws JSONException {
+        FeatureCollection featureCollection = (FeatureCollection) (geoJSON);
+        List<Feature> features = featureCollection.getFeatures();
+        int p = 0;
+        for (Feature feature : features) {
+            Country country = new Country(feature.getProperties().getString("sovereignt"));
+            List<PointF[]> border = new ArrayList<>();
+            Log.d("Country", country.getName() + " " + feature.getGeometry().getType());
+              /*  if (feature.getGeometry().getType() == "Point") {
+                    Point point = (Point) feature.getGeometry();
+                    point.getPosition().getLatitude();
+                    text += point.getPosition().getLatitude() + " " + point.getPosition().getLongitude() + " " + point.getPosition().getAltitude();
+                } else*/
+            if (feature.getGeometry().getType() == "Polygon") {
+                readPolygon((Polygon) feature.getGeometry(), border);
+                Log.d("Polygon", "");
+            } else if (feature.getGeometry().getType() == "MultiPolygon") {
+                MultiPolygon multiPolygon = (MultiPolygon) feature.getGeometry();
+                List<Polygon> polygons = multiPolygon.getPolygons();
+                for (Polygon polygon : polygons) {
+                    readPolygon(polygon, border);
+                }
+            }
+            Log.d("Points", "Points count " + p);
+            country.setBorder(border);
+            countries.add(country);
+        }
+        Toast.makeText(this, countries.get(0).getName(), Toast.LENGTH_LONG);
     }
 }
--- app/src/main/java/com/example/belarusgeogame/MyView.java
+++ app/src/main/java/com/example/belarusgeogame/MyView.java
@@ -5,43 +5,54 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.List;
 
 public class MyView extends View {
     private Paint paint;
     private Paint paintFill;
-    private Path path;
+    private List<Country> countries;
     private float scale = 1, dx, dy;
+    private Context context;
 
     public MyView(Context context) {
         super(context);
+        this.context = context;
         init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         init();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        this.context = context;
         init();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if (path != null) {
+        if (countries != null) {
             canvas.translate(dx, dy);
-            canvas.scale(scale, scale);
-
-            canvas.drawPath(path, paintFill);
-            canvas.drawPath(path, paint);
-
+            // canvas.scale(scale, scale);
+            for (Country country : countries) {
+                for (Path path : country.getPaths()) {
+                    canvas.drawPath(path, paintFill);
+                    canvas.drawPath(path, paint);
+                }
+            }
         }
     }
 
@@ -49,8 +60,8 @@ public class MyView extends View {
         setOnTouchListener(new OnTouchListener());
     }
 
-    public void addPath(Path path) {
-        this.path = path;
+    public void addCountries(List<Country> countries) {
+        this.countries = countries;
         paint = new Paint();
         paintFill = new Paint();
         paint.setColor(Color.BLUE);
@@ -64,6 +75,7 @@ public class MyView extends View {
     public class OnTouchListener implements View.OnTouchListener {
         float x = 0, y = 0;
         boolean move = false;
+        Toast toast = null;
 
         @Override
         public boolean onTouch(View v, MotionEvent event) {
@@ -82,8 +94,21 @@ public class MyView extends View {
                     move = true;
                 case MotionEvent.ACTION_UP:
                     if (!move) {
-                        if (x > 300) scale += 0.25;
-                        else scale -= 0.25;
+                        //Log.d("ACTION_UP", (dx) + " " + (dy));
+                        int s = 0;
+                        for (Country country : countries) {
+                            PointF p = new PointF(x - dx, y - dy);
+                            boolean b = Country.pip(p, country.getBorder());
+                            s++;
+                            if (b) {
+                                if (toast != null) toast.cancel();
+                                toast = Toast.makeText(getContext(), country.getName(), Toast.LENGTH_SHORT);
+                                toast.show();
+                                Log.d("PIP", country.getName());
+                                break;
+                            }
+                        }
+                        Log.d("N", "s=" + s);
                         invalidate();
                     }
                     break;
